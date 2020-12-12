0a1868245c8d1d267cd8b73b5af5e844b40857db
Hossein Akbari <hosseinakbari506@gmail.com>
2020-12-12 20:21:28 +0330

    little changes

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,59 +2,74 @@
   Carousel
 -------------------------------------------------------------------*/
 
-// Testimonials
-$('.section-item-3').each(function() {
-    var carousel = new Swiper('.section-item-3', {
-        slidesPerView: 2,
-        spaceBetween: 30,
-        //loop: true,
-        grabCursor: true,
-        watchOverflow: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
-        autoplay: {
-            delay: 5000,
-        },
-        breakpoints: {
-            991: {
-                slidesPerView: 1,
-                spaceBetween: 0
-            }
-        }
-    });
-});
-
-// Clients
-$('.js-carousel-clients').each(function() {
-    var carousel = new Swiper('.js-carousel-clients', {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        //loop: true,
-        grabCursor: true,
-        watchOverflow: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 0
-            },
-            580: {
-                slidesPerView: 2,
-                spaceBetween: 30
-            },
-            991: {
-                slidesPerView: 3,
-                spaceBetween: 30
-            }
-        }
-    });
-});
+// Login
 
 $('.message a').click(function() {
     $('form').animate({ height: "toggle", opacity: "toggle" }, "slow");
 });
+
+// Homepage
+
+function section_header_over_1() {
+    document.getElementById('section-header-boxs-icon-1').style.color = '#aae68b';
+}
+
+function section_header_out_1() {
+    document.getElementById('section-header-boxs-icon-1').style.color = '#29a58d';
+}
+
+function section_header_over_2() {
+    document.getElementById('section-header-boxs-icon-2').style.color = '#aae68b';
+}
+
+function section_header_out_2() {
+    document.getElementById('section-header-boxs-icon-2').style.color = '#29a58d';
+}
+
+function section_header_over_3() {
+    document.getElementById('section-header-boxs-icon-3').style.color = '#aae68b';
+}
+
+function section_header_out_3() {
+    document.getElementById('section-header-boxs-icon-3').style.color = '#29a58d';
+}
+
+function section_header_over_4() {
+    document.getElementById('section-header-boxs-icon-4').style.color = '#aae68b';
+}
+
+function section_header_out_4() {
+    document.getElementById('section-header-boxs-icon-4').style.color = '#29a58d';
+}
+
+function section_header_over_5() {
+    document.getElementById('section-header-boxs-icon-5').style.color = '#aae68b';
+}
+
+function section_header_out_5() {
+    document.getElementById('section-header-boxs-icon-5').style.color = '#29a58d';
+}
+
+function section_header_over_6() {
+    document.getElementById('section-header-boxs-icon-6').style.color = '#aae68b';
+}
+
+function section_header_out_6() {
+    document.getElementById('section-header-boxs-icon-6').style.color = '#29a58d';
+}
+
+function section_header_over_7() {
+    document.getElementById('section-header-boxs-icon-7').style.color = '#aae68b';
+}
+
+function section_header_out_7() {
+    document.getElementById('section-header-boxs-icon-7').style.color = '#29a58d';
+}
+
+function section_header_over_8() {
+    document.getElementById('section-header-boxs-icon-8').style.color = '#aae68b';
+}
+
+function section_header_out_8() {
+    document.getElementById('section-header-boxs-icon-8').style.color = '#29a58d';
+}
