c05a6a2d0716447ef8552594d85a1c20384dcefb
Hossein Akbari <hosseinakbari506@gmail.com>
2021-02-27 19:09:39 +0330

    Fix a bug in the login page

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 
 class Login extends Component {
+    constructor (props){
+        super(props);
+
+        this.state = {
+            expand: true
+        }
+    }
+
     render() {
         return (
-            <div class="section">
-                <div class="container-login">
-                    <div class="login-page">
-                        <div class="form">
-                            <form class="register-form">
-                                <input type="text" placeholder="نام و نام خانوادگی" />
-                                <input type="text" placeholder="ایمیل" />
-                                <input type="password" placeholder="پسورد" />
-                                <button>ثبت نام</button>
-                                <p class="message">قبلا ثبت نام کرده اید؟ <a href="#">وارد شوید</a></p>
-                            </form>
-                            <form class="login-form">
-                            <input type="text" placeholder="نام کاربری یا ایمیل" />
-                            <input type="password" placeholder="پسورد" />
-                            <button>ورود</button>
-                            <p class="message">هنوز ثبت نام نکرده اید؟ <a href="#">یک اکانت بسازید</a></p>
-                            </form>
+            <div>
+                <Helmet>
+                    <title>حساب کاربری</title>
+                </Helmet>
+                <div class="section">
+                    <div class="container-login">
+                        <div class="login-page">
+                            <div class="form">
+
+                                <div style={this.state.expand ? {display: 'none'} : { }}>
+                                    <form class="register-form">
+                                        <h4 style={{marginBottom: "25px"}}>ثبت نام در سایت</h4>
+                                        <input type="text" placeholder="نام و نام خانوادگی" />
+                                        <input type="text" placeholder="ایمیل" />
+                                        <input type="password" placeholder="پسورد" />
+                                        <button>ثبت نام</button>
+                                        <p class="message">قبلا ثبت نام کرده اید؟ <span onClick={() => { this.setState ( {expand: true}) }}>وارد شوید</span></p>
+                                    </form>
+                                </div>
+
+                                <div style={this.state.expand ? { } : {display: 'none'}}>
+                                    <form class="login-form">
+                                        <h4 style={{marginBottom: "25px"}}>ورود به حساب کاربری</h4>
+                                        <input type="text" placeholder="نام کاربری یا ایمیل" />
+                                        <input type="password" placeholder="پسورد" />
+                                        <button>ورود</button>
+                                        <p class="message">هنوز ثبت نام نکرده اید؟ <span onClick={() => { this.setState ( {expand: false}) }}>یک اکانت بسازید</span></p>
+                                    </form>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
