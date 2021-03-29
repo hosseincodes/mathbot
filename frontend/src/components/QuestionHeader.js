296c5e4573844ae07e645044e113e56f8fc55e0a
Hossein Akbari <hosseinakbari506@gmail.com>
2021-03-29 19:39:08 +0430

    Slightly improved the design of the question page and deleted several CSS codes from the past

--- frontend/src/components/QuestionHeader.js
+++ frontend/src/components/QuestionHeader.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class QuestionHeader extends Component {
     render () {
@@ -7,13 +8,13 @@ class QuestionHeader extends Component {
                 <h3>کمترین تعداد علامت جمع مورد نیاز برای نمایش حاصل عددی عبارت کدام است؟</h3>
                 <div className="row">
                     <div className="col-md-2 col-sm-4 col-xs-4 forum-title-ask">
-                        <h6>پرسیده شده در امروز</h6>
+                        <h6><Link className="username-answer" to="/users/ali">علی اکبری</Link></h6>
                     </div>
                     <div className="col-md-2 col-sm-4 col-xs-4 forum-title-view">
-                        <h6>17 بازدید</h6>
+                        <h6>20 اردیبهشت 1399</h6>
                     </div>
                     <div className="col-md-2 col-sm-4 col-xs-4 forum-title-last-seen">
-                           <h6>آخرین بازدید امروز</h6>
+                        <h6>3000 بازدید</h6>
                     </div>
                     <div className="col-md-6 col-xs-12"></div>
                 </div>
