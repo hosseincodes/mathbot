53e9f17ff9bf5bb4b1fe65f397d99f8b7afff040
Hossein Akbari <hosseinakbari506@gmail.com>
2021-04-07 23:09:05 +0430

    Header update (add search and login button)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,17 +5,35 @@ class Header extends Component {
     render () {
         return (
             <div className="header">
-                <div className="container">
-                   <div className="row">
-                        <div className="col-md-5 col-xs-3"></div>
-                            <div className="col-md-2 col-xs-6">
-                                <Link className="logo-button" to="/">
-                                    <span className="logo-button-span">م</span>ث <span className="logo-button-span">ب</span>ات 
-                                </Link>
-                                <span style={{fontSize: "10px"}}>(پیش آلفا)</span>
-                            </div>
-                        <div className="col-md-5 col-xs-3"></div>
+                <div className="row"> 
+
+                    <div className="col-md-4 col-xs-9">
+
+                        <Link className="logo-button" to="/">
+                            <span>انجمن پرسش و پاسخ ریاضی <span className="logo-button-span">م</span>ث <span className="logo-button-span">ب</span>ات</span>
+                        </Link>
+
                     </div>
+
+                    <div className="col-md-3"></div>
+
+                    <div className="col-md-3">
+
+                        {/* Fake Search Box */}
+                        <Link className="search-box">
+                            <i class="fas fa-search search-btn"></i><span>جستجو در سایت</span>
+                        </Link>
+
+                    </div>
+
+                    <div className="col-md-2 col-xs-3">
+
+                        <Link className="login-button" to="/login">
+                            <span>ورود</span>
+                        </Link>
+
+                    </div>
+
                 </div>
             </div>
         );
