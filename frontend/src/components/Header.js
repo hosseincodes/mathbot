4d988c51db778cf2c3f938b84c42eb748b39b81f
Hossein Akbari <hosseinakbari506@gmail.com>
2021-04-08 11:47:24 +0430

    Minor changes in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,28 +9,32 @@ class Header extends Component {
 
                     <div className="col-md-4 col-xs-9">
 
-                        <Link className="logo-button" to="/">
+                        <div className="logo-button">
+                        <Link to="/">
                             <span>انجمن پرسش و پاسخ ریاضی <span className="logo-button-span">م</span>ث <span className="logo-button-span">ب</span>ات</span>
                         </Link>
+                        </div>
 
                     </div>
 
-                    <div className="col-md-3"></div>
+                    <div className="col-md-6">
 
-                    <div className="col-md-3">
-
-                        {/* Fake Search Box */}
-                        <Link className="search-box">
+                        <div className="search-box-big">
+                            {/* Fake Search Box */}
+                        <div className="search-box">
                             <i class="fas fa-search search-btn"></i><span>جستجو در سایت</span>
-                        </Link>
+                        </div>
+                        </div>
 
                     </div>
 
                     <div className="col-md-2 col-xs-3">
 
-                        <Link className="login-button" to="/login">
+                        <div className="login-button">
+                        <Link to="/login">
                             <span>ورود</span>
                         </Link>
+                        </div>
 
                     </div>
 
