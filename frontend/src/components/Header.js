3e30d818aebccbbcc37f8b8bdb1dd9995b63393b
Hossein Akbari <hosseinakbari506@gmail.com>
2021-06-01 10:17:10 +0430

    Minor changes in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,34 +5,33 @@ class Header extends Component {
     render () {
         return (
             <div className="header">
-                <div className="row"> 
+                <div className="row">
 
-                    <div className="col-md-4 col-xs-9">
-                        <div className="logo-button">
-                            <Link to="/">
-                                <span>انجمن پرسش و پاسخ ریاضی <span className="logo-button-span">م</span>ث <span className="logo-button-span">ب</span>ات</span>
+                    <div className="col-md-3 col-xs-3">
+                        <div className="login-button">
+                            <Link to="/login">
+                                <span>ورود به حساب کاربری</span>
                             </Link>
                         </div>
-                    </div>
+                    </div> 
 
-                    <div className="col-md-6">
+                    <div className="col-md-7">
                         <div className="search-box-big">
                             <div className="wrap">
                                 <div className="search">
                                     <div className="searchTerm">
-                                    <span><i className="fa fa-search search-icon"></i></span>
-                                    <input type="text" placeholder="Search"/>
-                                    </div>
-                                    
+                                        <span><i className="fa fa-search search-icon"></i></span>
+                                        <input type="text" placeholder="Search"/>
+                                    </div> 
                                 </div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="col-md-2 col-xs-3">
-                        <div className="login-button">
-                            <Link to="/login">
-                                <span>ورود</span>
+                    <div className="col-md-2 col-xs-9">
+                        <div className="logo-button">
+                            <Link to="/">
+                                <span>mathbot</span>
                             </Link>
                         </div>
                     </div>
