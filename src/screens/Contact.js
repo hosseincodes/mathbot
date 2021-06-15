aba3af214a9681be153b4687584698a5948b8385
Hossein Akbari <hosseinakbari506@gmail.com>
2021-06-15 10:48:02 +0430

    Contact page update (LINKS)

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -16,21 +16,27 @@ class Contact extends Component {
 
                 <div class="section">
                     <div class="contact">
+
                         <h1 class="contact-h1">تماس با ما</h1>
-                        <p class="contact-p">مث بات هنوز در ابتدای کار است و ما به شدت به نظرات و انتقادات شما نسبت به این وب سایت نیازمنیدم.</p>
-                    <div class="contact-div">
-                        <a class="contact-a" href="mailto:hosseinakbari506@gmail.com "><i class="fas fa-envelope "></i></a>
-                        <a class="contact-a" href="tel:+989156179233"><i class="fas fa-phone "></i></a>
-                        <a class="contact-a" href="t.me/ho0o0o3ein"><i class="fab fa-telegram-plane"></i></a>
-                    </div>
-                        <p class="contact-p">مث بات اپن سورس است و اگر توانایی کمک به توسعه آن را دارید، در گیت هاب با ما همراه باشید.</p>
-                    <div class="contact-div">
-                        <a class="contact-a" href="http://github.com/hosseincodes/mathbot "><i class="fab fa-github "></i></a>
-                    </div>
-                        <p class="contact-p">درشبکه های اجتماعی</p>
-                    <div class="contact-div">
-                    <a class="contact-a" href="http://instagram.com/mathbot.ir "><i class="fab fa-instagram "></i></a>
-                    </div>
+
+                            <p class="contact-p">مث بات هنوز در ابتدای کار است و ما به شدت به نظرات و انتقادات شما نسبت به این پروژه نیازمنیدم.</p>
+                        <div class="contact-div">
+                            <a class="contact-a" href="mailto:hosseinakbari506@gmail.com "><i class="fas fa-envelope "></i></a>
+                            <a class="contact-a" href="http://t.me/ho0o0o3ein"><i class="fab fa-telegram-plane"></i></a>
+                        </div>
+
+                            <p class="contact-p">کد های سمت کاربری مث بات اپن سورس است و اگر توانایی کمک به توسعه آن را دارید، در گیت هاب با ما همراه باشید</p>
+                        <div class="contact-div">
+                            <a class="contact-a" href="http://github.com/hosseincodes/mathbot "><i class="fab fa-github "></i></a>
+                        </div>
+
+                            <p class="contact-p">درشبکه های اجتماعی</p>
+                        <div class="contact-div">
+                            <a class="contact-a" href="http://facebook.com/officialmathbot "><i class="fab fa-facebook "></i></a>
+                            <a class="contact-a" href="http://instagram.com/themathbot "><i class="fab fa-instagram "></i></a>
+                            <a class="contact-a" href="http://t.me/math_20_bot "><i class="fab fa-telegram "></i></a>
+                        </div>
+
                     </div>
                 </div>
 
