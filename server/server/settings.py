ccf6bf1574eaf2790a65c5484e255e2728717627
Hossein Akbari <hosseinakbari506@gmail.com>
2024-03-18 12:54:55 +0330

    adding drf and django apps to backend settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -37,7 +37,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    'accounts',
+    'comments',
+    'posts'
 ]
+
+# REST_FRAMEWORK
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
