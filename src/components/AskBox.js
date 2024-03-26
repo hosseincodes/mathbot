780506d6e222c7ebca6171066670bbc45d15da9f
Hossein Akbari <hosseinakbari506@gmail.com>
2024-03-26 14:01:11 +0330

    improve the design of the question page

--- src/components/AskBox.js
+++ src/components/AskBox.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import RichEditor from './RichEditor';
+// import RichEditor from './RichEditor';
 
 function AskBox() {
 
@@ -20,18 +20,21 @@ function AskBox() {
         try {
           const response = await axios.post("https://server.mathbot.ir/api/posts/create/", formData);
           console.log("Post created:", response.data);
+          alert("پست با موفقیت آپلود شد");
         } catch (error) {
           console.error("Error creating post:", error.response.data);
+          alert("پست آپلود نشد! یه مشکلی وجود داره")
         }
       };
 
     return (
         <div className="ask-box">
-            {/* <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <div className="ask-title">
                     <h4>نام سوال</h4>
                     <input
-                        value={data.title}
+                        name="title"
+                        value={formData.title}
                         onChange={handleChange}
                         className="ask-input-title"
                         placeholder="مثلا کمترین تعداد علامت جمع مورد نیاز برای نمایش حاصل عددی عبارت کدام است؟"
@@ -40,33 +43,19 @@ function AskBox() {
 
                 <div classNameName="ask-description">
                     <h4>توضیحات</h4>
-                    <RichEditor
+                    {/* <RichEditor
                         value={data.content}
                         onChange={handleChange}
-                    />
+                    /> */}
+                    <textarea className='ask-description-textarea' name="content" value={formData.content} onChange={handleChange}></textarea>
                 </div>
-                <div className="ask-tags">
+                {/* <div className="ask-tags">
                     <h4>برچسب ها</h4>
                     <textarea className="ask-input-tags" placeholder="مثلا انتگرال ..."></textarea>
-                </div>
+                </div> */}
                 <div className="ask-button">
                     <button className="ask-input-button" type="submit">ارسال سوال</button>
                 </div>
-            </form> */}
-        
-
-        <form onSubmit={handleSubmit}>
-            <label>
-            Title:
-            <input type="text" name="title" value={formData.title} onChange={handleChange} />
-            </label>
-            <br />
-            <label>
-            Body:
-            <textarea name="content" value={formData.content} onChange={handleChange}></textarea>
-            </label>
-            <br />
-            <button type="submit">Add Post</button>
             </form>
         </div>
     );
