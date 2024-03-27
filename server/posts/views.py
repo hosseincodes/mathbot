99c1558cfe65525ad5efbf6a05638528f1dbd7b9
Hossein Akbari <hosseinakbari506@gmail.com>
2024-03-27 21:50:25 +0330

    Changes to pagination (disabling public mode)

--- server/posts/views.py
+++ server/posts/views.py
@@ -2,12 +2,19 @@ from rest_framework import generics
 from .models import Post
 from django.contrib.auth import get_user_model
 from .serializers import PostSerializer, UserSerialzier
+from rest_framework.pagination import PageNumberPagination
 
 User = get_user_model()
+
+class LargeResultsSetPagination(PageNumberPagination):
+    page_size = 30
+    page_size_query_param = 'page_size'
+    max_page_size = 30
 
 class PostsListAPIView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = LargeResultsSetPagination
 
 class PostsCreateAPIView(generics.CreateAPIView):
     queryset = Post.objects.all()
--- server/server/settings.py
+++ server/server/settings.py
@@ -51,8 +51,6 @@ INSTALLED_APPS = [
 
 # REST_FRAMEWORK
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 30,
     # 'DEFAULT_PERMISSION_CLASSES': (
     #     'rest_framework.permissions.IsAuthenticated',
     # ),
