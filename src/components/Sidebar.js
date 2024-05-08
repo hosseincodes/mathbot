64607ea6091791ec24ebf713dc63d6519d52c226
Hossein Akbari <hosseinakbari506@gmail.com>
2024-05-08 22:35:14 +0330

    remove popular tags from sidebar and put a login alert instead

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,26 @@
 import React from "react";
 import IsAuthenticated from "../utils/IsAuthenticated";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
+
+    const username = IsAuthenticated()
     
     return (
         <>
-            {IsAuthenticated() === "Not Authenticated" ? (<></>) : (
+            {username === "Not Authenticated" ? (
+                <Link to="/login">
+                    <div className="sidebar login-text-green">
+                        <p>برای ثبت سوال یا پاسخ دادن به سوال های بقیه وارد حساب کاربری خود شوید</p>
+                    </div>
+                </Link>
+            ) : (
                 <div className="sidebar">
-                    <p><i class="fa-solid fa-clipboard-question"></i> سوال های من</p>
-                    <p><i class="fa-regular fa-bookmark"></i> بعدا حل می کنم</p>
-                    <p><i class="fa-regular fa-thumbs-up"></i> سوال های مورد علاقه</p>
+                    <Link className="sidebar-user-links" to={`/users/${username}`}><p><i class="fa-solid fa-user"></i> پروفایل من</p></Link>
+                    <Link className="sidebar-user-links" to="/soon"><p><i class="fa-regular fa-bookmark"></i> بعدا حل می کنم</p></Link>
+                    <Link className="sidebar-user-links" to="/soon"><p><i class="fa-regular fa-thumbs-up"></i> سوال های مورد علاقه</p></Link>
                 </div>
             )}
-            <div className="sidebar">
-                <p>موضوعات داغ!</p>
-                <p>کنکور</p>
-                <p>انتگرال</p>
-                <p>مشتق</p>
-                <p>ریاضی ۲</p>
-            </div>
             <div className="sidebar">
                 <p>تبلیغات</p>
             </div>
