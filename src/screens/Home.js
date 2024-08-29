ea36476cbb00ff2538f147c23cd677cc8bd71bec
Hossein Akbari <hosseinakbari506@gmail.com>
2024-08-29 18:53:53 +0330

    unused link removed

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,7 +4,6 @@ import { Helmet } from 'react-helmet';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
-import { Link } from "react-router-dom";
 
 function Home() {
 
