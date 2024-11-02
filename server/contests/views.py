18c50469c49191ae4073697f6c3a38a04d8bdda8
Hossein Akbari <hosseinakbari506@gmail.com>
2024-11-02 12:53:34 +0330

    create contest serializer and update models

--- server/contests/views.py
+++ server/contests/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Contest
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+
+class ContestsCreateAPIView(generics.CreateAPIView):
+    queryset = Contest.objects.all()
+    # serializer_class = ContestCreateSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
