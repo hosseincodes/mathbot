b253251c2afc3c2b9b112aacb5b0b0e83fa2dbfd
Hossein Akbari <hosseinakbari506@gmail.com>
2024-11-07 21:15:48 +0330

    add teams model fields

--- server/teams/models.py
+++ server/teams/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
+class Team(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name="teams")
+    avatar = models.ImageField(upload_to='teamsAvatar')
+    members = models.ForeignKey(
+        User,
+            on_delete = models.CASCADE,
+            related_name='teams'
+        )
