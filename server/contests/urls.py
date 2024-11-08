88a0f52a71c7d1b8fd7606abe990664c97b0351a
Maziar2003 <maziarheidari1124@gmail.com>
2024-11-08 12:23:39 +0330

    some unnecessary classes from the contests.views are commented

--- server/contests/urls.py
+++ server/contests/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.ContestsCreateAPIView.as_view(), name='contest-create'),
+    # path('create/', views.ContestsCreateAPIView.as_view(), name='contest-create'),
     path('<int:pk>/', views.ContestsDetailAPIView.as_view(), name='contest-detail'),
-    path('<int:pk>/delete/', views.ContestsDeleteAPIView.as_view(), name='contest-delete'),
+    # path('<int:pk>/delete/', views.ContestsDeleteAPIView.as_view(), name='contest-delete'),
 ]
--- server/contests/views.py
+++ server/contests/views.py
@@ -5,18 +5,20 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from ..permissions import IsOwnerOrAdmin
 
 
-class ContestsCreateAPIView(generics.CreateAPIView):
-    queryset = Contest.objects.all()
-    # serializer_class = ContestCreateSerializer
-    permission_classes = [IsAuthenticated]
-    authentication_classes = [JWTAuthentication]
+# class ContestsCreateAPIView(generics.CreateAPIView):
+#     queryset = Contest.objects.all()
+#     # serializer_class = ContestCreateSerializer
+#     permission_classes = [IsAuthenticated]
+#     authentication_classes = [JWTAuthentication]
+
+
 
 class ContestsDetailAPIView(generics.RetrieveAPIView):
     queryset = Contest.objects.all()
     # serializer_class = PostSerializer
 
-class ContestsDeleteAPIView(generics.DestroyAPIView):
-    queryset = Contest.objects.all()
-    # serializer_class = PostSerializer
-    permission_classes = [IsOwnerOrAdmin]
-    authentication_classes = [JWTAuthentication]
+# class ContestsDeleteAPIView(generics.DestroyAPIView):
+#     queryset = Contest.objects.all()
+#     # serializer_class = PostSerializer
+#     permission_classes = [IsOwnerOrAdmin]
+#     authentication_classes = [JWTAuthentication]
