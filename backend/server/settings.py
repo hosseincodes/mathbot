ed0140783c18fffaa598f3303fa91d507de511e9
Hossein <hosseinakbari506@gmail.com>
2024-12-15 17:31:47 +0330

    fix .env file routing bug

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from decouple import config
 
-config = Config('.env.development.local')
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
